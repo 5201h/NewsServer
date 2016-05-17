e1a8f1910ce19b5f242edcf83e98f9de1c9d84e9
5201h <y5201h@hotmail.com>
2016-05-18 01:53:21 +0800

    first commit and push

---
+++ src/com/lg/servlet/LoginServlet.java
@@ -0,0 +1,53 @@
+package com.lg.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if (username != null || "".equals(username) || password != null || "".equals(password)) {
+			if ("001".equals(username) && "lis001".equals(password)) {
+				response.getOutputStream().write("login success".getBytes());
+			} else {
+				response.getOutputStream().write("login fail".getBytes());
+			}
+		} else {
+			response.getOutputStream().write("login fail: username or password is null.".getBytes());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
